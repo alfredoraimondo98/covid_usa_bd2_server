7ef0449fc1cacd98a84333761ad8fed0d534f1d8
Alfredo <alfredoraimondo98@gmail.com>
2021-07-12 11:14:34 +0200

    integrationQuery aggregate

--- controllers/integrationQuery.js
+++ controllers/integrationQuery.js
@@ -16,7 +16,10 @@ exports.getAllData = async (req, res, next) => {
     specializzazioni = req.body.specializzazioni;
 
 
+    var condition = {}
+    var projGroup = {} //group by mongo (in .aggregate)
     
+
 //***FASE 1: PROIEZIONE */
     //Creazione proiezione 
     var projection = {};
@@ -25,21 +28,25 @@ exports.getAllData = async (req, res, next) => {
         if(elProject.field == '_id'){ //Verifica se il campo è _id allora inserire sia con 1 (true) che con 0 (false)
             if(elProject.checked){
                 projection[elProject.field.toString()] = 1
+                projGroup[elProject.field.toString()] = "$"+elProject.field.toString();
             }
             else{
                 projection[elProject.field.toString()] = 0
             }
         } //Altrienti per tutti gli altri valori si procede all'inserimento soltanto se checked = 1 (true)
         else if(elProject.checked){
+           // group={}
             projection[elProject.field.toString()] = 1
-        }
+            projGroup[elProject.field.toString()] = "$"+elProject.field.toString();
+            
+            
+            condition[elProject.field.toString()] = { $exists : true } //richiede che i campi non oggetto di condizione debbano esistere nel risultato della query
+         
+        } 
 
     })
 
-    //console.log(" PROJ ", projection)
-
-
-  
+   
    
 
 /**FASE 2: QUERY (CONDIZIONI) */
@@ -54,7 +61,6 @@ exports.getAllData = async (req, res, next) => {
     {"byDataFine" :"2020-05-08 "}
     , */
 
-    var condition = {}
         //verifica il criterio di ricerca (State, County, City)
         if(condizioni.searchBy){
             if(condizioni.searchBy.type == 'Stato' && condizioni.searchBy.value != 'Tutti' ){
@@ -94,100 +100,42 @@ exports.getAllData = async (req, res, next) => {
   		}
     }, */
 
-    //console.log(" *** ", specializzazioni)
-/*
-    if(specializzazioni[0].air_quality){ //condizioni aggiuntive su qualità dell'aria
-        if(specializzazioni[0].air_quality.start && specializzazioni[0].air_quality.end){ //Se entrambi i parametri (min, max) sono definiti
-            condition['cities_air_quality.air_quality'] = {
-                $gte : +specializzazioni[0].air_quality.start,
-                $lte : +specializzazioni[0].air_quality.end
-            }
-        }
-        else if(specializzazioni[0].air_quality.start){ //se è definito solo lo start (maggiore di)
-                condition['cities_air_quality.air_quality'] = {
-                    $gte : +specializzazioni[0].air_quality.start,
-                }
-            }
-            else if(specializzazioni[0].air_quality.end){ //Se è definito solo l'end (minore di)
-                condition['cities_air_quality.air_quality'] = {
-                    $lte : +specializzazioni[0].air_quality.end,
-                }
-            }
-    }
+    specializzazioniCovid(specializzazioni, condition); //Specializzazioni covid19
 
-    //Covid-19 Specializzazione
-    if(specializzazioni[1].byCasiCovid){ //condizioni aggiuntive sui "casi covid"
-        if(specializzazioni[1].byCasiCovid.start && specializzazioni[1].byCasiCovid.end){ //Se entrambi i parametri (min, max) sono definiti
-            condition['cases'] = {
-                $gte : +specializzazioni[1].byCasiCovid.start,
-                $lte : +specializzazioni[1].byCasiCovid.end
-            }
-        }
-        else if(specializzazioni[1].byCasiCovid.start){ //se è definito solo lo start (maggiore di)
-                condition['cases'] = {
-                    $gte : +specializzazioni[1].byCasiCovid.start,
-                }
-            }
-            else if(specializzazioni[1].byCasiCovid.end){ //Se è definito solo l'end (minore di)
-                condition['cases'] = {
-                    $lte : +specializzazioni[1].byCasiCovid.end,
-                }
-            }
-    }
+    specializzazioniLockdown(specializzazioni, condition); //specializzazioni Lockdown
+
+    specializzazioniAirQuality(specializzazioni, condition); //Specializzazioni airQuality
     
-
-    if(specializzazioni[2].byMortiCovid){ //condizioni aggiuntive sui "morti covid"
-        if(specializzazioni[2].byMortiCovid.start && specializzazioni[2].byMortiCovid.end){ //Se entrambi i parametri (min, max) sono definiti
-            condition['deaths'] = {
-                $gte : +specializzazioni[2].byMortiCovid.start,
-                $lte : +specializzazioni[2].byMortiCovid.end
-            }
-        }
-        else if(specializzazioni[2].byMortiCovid.start){ //se è definito solo lo start (maggiore di)
-                condition['cadeathsses'] = {
-                    $gte : +specializzazioni[2].byMortiCovid.start,
-                }
-            }
-            else if(specializzazioni[2].byMortiCovid.end){ //Se è definito solo l'end (minore di)
-                condition['deaths'] = {
-                    $lte : +specializzazioni[2].byMortiCovid.end,
-                }
-            }
-    }
-
-    //specializzazione lockdown
-    if(specializzazioni[3].type_lockdown){ //condizioni aggiuntive sui "casi covid"
-        condition['lockdown'] = specializzazioni[3].type_lockdown
-    }
-    
-    */
-
-    
-
-   // if(req.body.typeQuery = 'covid19'){
-        specializzazioniCovid(specializzazioni, condition)
-    //}
-    //if(req.body.typeQuery = 'lockdown'){
-        specializzazioniLockdown(specializzazioni, condition);
-
-    //}
-    //if(req.body.typeQuery = 'airQuality'){
-        specializzazioniAirQuality(specializzazioni, condition);
-    //}
-
-    console.log("condition " , condition);
 
 
     MongoClient.connect(url, async function(err, db) {
         if (err) throw err;
         var dbo = db.db("basi2");
 
-        dbo.collection("integrazioneFinale").find(condition).project(projection).toArray(async function(err, result) {
+
+
+        console.log("***QUERY: ", "$match (find):", condition , "\n project: ", projGroup, "\n group : {group : { _id : ", projGroup, "}} **" )
+
+       dbo.collection("integrazioneFinale").aggregate([
+           
+            {
+                "$match" :  condition  //find() 
+            },
+            {
+                "$project" : projection //project()
+            },
+            {
+                "$group": { //groupby
+                    "_id": projGroup
+                }
+            },
+        ]).toArray(async function(err, result) {
             if(err) throw err;
             console.log(result);
 
             db.close();
 
+            
             if(result.length > 0){ 
                 return res.status(201).json({
                     result : result
@@ -196,10 +144,12 @@ exports.getAllData = async (req, res, next) => {
             else{
                 return res.status(204).json({})
             }
-        });  
+        });
     })
-
 }
+
+
+
 
 
 function specializzazioniCovid(specializzazioni, condition){
